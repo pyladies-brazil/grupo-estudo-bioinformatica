2b86b55b903363a28d8f986f6a263f29500421e3
thayanavt <thayana.vt@gmail.com>
2020-10-24 11:15:18 -0300

    Create Exercicio_B_Thayana.py

---
+++ Fase 1/Exercicio_B_Thayana.py
@@ -0,0 +1,11 @@
+file_name = input('What is the file name?')
+file_name_result = file_name.replace('.txt', '_result')
+
+with open(file_name) as data:
+    nucleotides_DNA = data.read()
+    nucleotides_DNA.upper()
+    nucleotides_DNA = nucleotides_DNA.replace('T', 'U')
+    print(nucleotides_DNA)
+    
+with open(file_name_result, 'w') as data:
+    data.write(nucleotides_DNA)
