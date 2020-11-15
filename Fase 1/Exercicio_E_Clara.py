079140771244dd72fa07bca69b1ac01aa1421385
Clara Matos <euodeiofisica@hotmail.com>
2020-11-15 15:46:56 -0300

    Create Exercicio_E_Clara.py

---
+++ Fase 1/Exercicio_E_Clara.py
@@ -0,0 +1,13 @@
+#-----------Encontro 3-------------#
+
+     
+def contar_mutacoes_pontuais(seq1,seq2):
+    num = 0
+    for n,i in zip(seq1,seq2):
+        if n!=i:
+            num+=1
+    return num
+    #return sum(n!= i for n,i in zip(seq1,seq2))
+
+
+#print(contar_mutacoes_pontuais("GAGCCTACTAACGGGAT","CATCGTAATGACGGCCT"))
