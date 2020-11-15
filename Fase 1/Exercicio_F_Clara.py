981568c82325f639207af48d5eecc937691a8363
Clara Matos <euodeiofisica@hotmail.com>
2020-11-15 15:59:55 -0300

    Create Exercicio_F_Clara.py
    Rabbits and recurrence

---
+++ Fase 1/Exercicio_F_Clara.py
@@ -0,0 +1,15 @@
+#---------Encontro 3--------------#
+
+# months k-pairs
+
+def fibonacy_rabits(m, k):
+  child = 1
+  adults = 0
+  for n in range(m-1):
+     temp = adults*k
+     adults += child
+     child = temp
+     #child, adult = adults*k, adults+=child
+  return adults+child
+
+fibonacy_rabits(31, 4)
