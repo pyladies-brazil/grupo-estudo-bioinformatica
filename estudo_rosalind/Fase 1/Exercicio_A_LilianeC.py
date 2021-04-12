e5a64b623d357d602ed2fdc9e129d22245102aa5
lconteville <70072309+lconteville@users.noreply.github.com>
2021-04-12 09:16:13 -0300

    Add files via upload

---
+++ estudo_rosalind/Fase 1/Exercicio_A_LilianeC.py
@@ -0,0 +1,12 @@
+# Counting DNA Nucleotides
+
+dataset = input("Insira a sequência nucleotídica:")
+
+print(dataset.count('A'), dataset.count('C'), dataset.count('G'), dataset.count('T'))
+
+
+# Transcribing DNA into RNA 
+
+dataset = input("Insira a sequência nucleotídica:")
+
+print(dataset.replace("T", "U"))
