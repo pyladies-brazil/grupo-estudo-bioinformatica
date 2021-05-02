51fa16a7d766267acfd798638c13edc5677247e5
thayanavt <thayana.vt@gmail.com>
2021-05-02 11:34:16 -0300

    Update Exercicio_E_Thayana.py

--- estudo_rosalind/Fase 2/Exercicio_E_Thayana.py
+++ estudo_rosalind/Fase 2/Exercicio_E_Thayana.py
@@ -1,26 +1,26 @@
-import math
 
-k = int(input("Type the number of homozygous dominant: "))
-m = int(input("Type the number of heterozygous: "))
-n = int(input("Type the number of homozygous recessive: "))
+from Bio import SeqIO, ExPASy
+import os
+import time
 
-population = k + m + n
+path = os.path.dirname(__file__)
+ids_file = open(rf'{path}\Rosalind.txt', 'r')
+ids_list = [line.strip() for line in ids_file]
 
-#combination formula
-total_combination = factorial(population)/(factorial(2)*factorial((population-2)))
+def extract_sequence_biopython(id):
+    with ExPASy.get_sprot_raw(id) as handle:
+        seq_record = SeqIO.read(handle, "swiss")
+    return seq_record.seq
 
-if k>=2:
-  AA_combinations =  factorial(k)/(factorial(2)*factorial((k-2))) + (k * m) + (k * n)
-  AaAa_combinations = factorial(m)/(factorial(2)*factorial((m-2)))
-  Aaaa_combinations = (m * n)
-elif k==1:
-  AA_combinations = (k * m) + (k * n)
-
-prob_AA_combinations_dominant = AA_combinations / total_combination
-prob_AaAa_combinations_dominant = (AaAa_combinations / total_combination) * (3/4)
-prob_Aaaa_combinations_dominant = (Aaaa_combinations / total_combination) * (2/4)
-
-total_combinations_dominant = prob_AA_combinations_dominant + prob_AaAa_combinations_dominant + prob_Aaaa_combinations_dominant
-print("{0:.5f}".format(total_combinations_dominant))
-
-
+def find_Motif(seq_id, seq):
+    indexMotif_string = ""
+    for i in range(len(seq)):
+        if (seq[i] == 'N') and (seq[i+2] == 'S' or seq[i+2] == 'T'):
+            if seq[i+1] != 'P' and seq[i+3] != 'P':
+                indexMotif_string += f"{i+1} "
+    if len(indexMotif_string) != 0:
+        print(f"{id}\n{indexMotif_string}")
+        
+for id in ids_list:
+    seq = extract_sequence_biopython(id)
+    find_Motif(id, seq)
